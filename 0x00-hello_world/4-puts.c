44d6342317d1b84fb2ca0414666c74660817ce2e
samah-EC3 <samah.ahmed.ec3@gmail.com>
2023-06-15 13:57:40 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"); /*print to standard output*/
 return (0);
 }
