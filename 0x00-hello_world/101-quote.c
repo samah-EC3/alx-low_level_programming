f9d8aa85ce8266f1b02ada7fc79c32c0f1b4811e
samah-EC3 <samah.ahmed.ec3@gmail.com>
2023-06-15 15:01:39 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+/**
+* main - entry point
+* Description: c program use puts to print
+* Return: always one if error
+*/
+int main(void)
+{
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
+	write(1, text, 59);
+	return (1);
+}
