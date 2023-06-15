f869c1f022e547f28fb00bda411d32b53356c423
samah-EC3 <samah.ahmed.ec3@gmail.com>
2023-06-15 14:20:28 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Description: c program use printf to print
+* Return: always zero if success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
