06f044b1a3683a796786758efedb5022238cff9b
samah-EC3 <samah.ahmed.ec3@gmail.com>
2023-06-20 14:47:09 -0700

    first putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include "main.h"
 /**
  * main - check the code
  * Description:print _putchar
@@ -7,6 +7,13 @@
 
 int main(void)
 {
-	printf("_putchar \n");
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch <= 8; ch++)
+	{
+		_putchar(str[ch]);
+	}
+	_putchar('\n');
 	return (0);
 }
