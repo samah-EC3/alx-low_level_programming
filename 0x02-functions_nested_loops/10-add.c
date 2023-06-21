b3944ef004ff4c47444e91cd44b57f7fa076ff03
samah-EC3 <samah.ahmed.ec3@gmail.com>
2023-06-21 14:58:23 -0700

    add two number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - take two number
+ * @a: first parameter
+ * @b: second parameter
+ * Return: sum of two number
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
