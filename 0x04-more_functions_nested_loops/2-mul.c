bfa0d9a1aacb1c196a6af04262937a7f5d06e7e6
samah-EC3 <samah.ahmed.ec3@gmail.com>
2023-06-22 14:15:19 -0700

    mult

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul- take two number
+ * @a: first parameter
+ * @b: second parameter
+ * Return: sum of two number
+ */
+
+int mul(int a, int b)
+{
+	int mult = a * b;
+
+	return (mult);
+}
