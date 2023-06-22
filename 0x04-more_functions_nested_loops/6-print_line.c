cc10057dadb1d6bed8549aed84de92bef9d6464b
samah-EC3 <samah.ahmed.ec3@gmail.com>
2023-06-22 15:25:44 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * print_line - print - according to n
+ * @n: parameter repeat character -
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('-');
+		}
+		_putchar('\n');
+	}
+}
