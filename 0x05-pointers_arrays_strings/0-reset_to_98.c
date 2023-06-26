11693fce7d060f64028bacd64895af6709af380a
samah-EC3 <samah.ahmed.ec3@gmail.com>
2023-06-26 12:31:03 -0700

    0-reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-/*
- * reset_to_98 -  take pointer to as input parameter and update
- *                  the value it point to 98
- * @n: an input parameter
- * Return: nothing
+/**
+ * reset_to_98 - Reset the parameter to 98
+ *@n: Pointe to the value we want to set to 98
+ *Return : void
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
