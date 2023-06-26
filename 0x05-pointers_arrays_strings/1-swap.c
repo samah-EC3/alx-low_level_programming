82b136c4c7f6956db16244adf055b87eea228910
samah-EC3 <samah.ahmed.ec3@gmail.com>
2023-06-26 12:44:06 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *swap_int - swap two integers
+ *@a: Pointer to the integer to be swaped with a
+ *@b: Pointer to the interget to be swaped with b
+ *Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+
+}
