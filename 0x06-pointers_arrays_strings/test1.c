c498ffcd27ccc9b0987f74295a42c89bc2e55ad3
samah-EC3 <samah.ahmed.ec3@gmail.com>
2023-06-29 14:28:01 -0700

    strncat

--- 0x06-pointers_arrays_strings/test1.c
+++ 0x06-pointers_arrays_strings/test1.c
@@ -9,24 +9,15 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int m;
-	int i;
+	int a = 0, i;
 
-	m = 0;
-
-	for (i = 0; i < 1000; i++)
-	{
-		if (dest[i] == '\0')
-		{
-			break;
-		}
-		m++;
-	}
-
-	for (i = 0; src[i] != '\0' && i < n; i++)
-	{
-		dest[m + i] = src[i];
-	}
-	dest[m + i] = '\0';
+	for (i = 0; dest[i] != '\0'; i++)
+	;
+	do {
+		dest[i] = src[a];
+		i++;
+		a++;
+	} while (src[a] != '\0' && a < n);
+	dest[i + a] = '\0';
 	return (dest);
 }
