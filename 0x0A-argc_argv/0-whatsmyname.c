8527b90edbe66998199933344ba72a3cb84276e9
samah-EC3 <samah.ahmed.ec3@gmail.com>
2023-07-07 15:09:40 -0700

    argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 
 /**
 * main - prints program name
-*@argc: number of arguments
-*@argv: array of arguments
+*@argc: int
+*@argv: list
 *Return: 0
 */
 int main(int argc, char const  *argv[])
 {
-	(void)argc; 
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
